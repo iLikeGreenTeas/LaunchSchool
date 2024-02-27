0b9d1d545277efa0433e5ccddd46101765331b1a
Abdul Hussain <abdulhussainsam@gmail.com>
2024-02-27 21:59:12 +0000

    Working in new branch assignment 24 RPS Bonus Features

--- RB101/Lesson-2_Small-Programs/assignment_21_rps.rb
+++ RB101/Lesson-2_Small-Programs/assignment_21_rps.rb
@@ -1,13 +1,23 @@
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(rock paper scissors spock lizard)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  game_winner_hash = {
+    rock: ["lizard", "scissors"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    spock: ["scissors", "rock"],
+    lizard: ["paper", "spock"]
+  }
+  
+  (first == 'rock' && game_winner_hash[:rock].include?(second)) ||
+    (first == 'paper' && game_winner_hash[:paper].include?(second)) ||
+    (first == 'scissors' && game_winner_hash[:scissors].include?(second)) ||
+    (first == 'spock' && game_winner_hash[:spock].include?(second)) ||
+    (first == 'lizard' && game_winner_hash[:lizard].include?(second))
 end
 
 def display_results(player, computer)
@@ -24,11 +34,13 @@ loop do
   choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
-
-    if VALID_CHOICES.include?(choice)
+    choice = Kernel.gets().downcase().chomp()
+    
+    if choice == 's'
+      prompt("Invalid: multiple options begin with 's': 'spock' and 'scissors'")
+    elsif (choice != 's') && (VALID_CHOICES.any? { |options| options.start_with?(choice) })
       break
-    else
+    elsif VALID_CHOICES.none? { |options| options.start_with?(choice) }
       prompt("That's not a valid choice.")
     end
   end
