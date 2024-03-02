6dc1a5e0a525c59799ac9f8e61b6383bb672453f
iLikeGreenTeas <abdulhussainsam@gmail.com>
2024-03-02 20:50:01 +0000

    Progress Updates

---
+++ RB101/lesson_3_practice_problems/easy_1/question_1.rb
@@ -0,0 +1,10 @@
+# What would you expect the code below to print out?
+
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+# Answer: This code will output the original array: [1, 2, 2, 3]
+
+# Result: Incorrect!
---
+++ RB101/lesson_3_practice_problems/easy_1/question_2.rb
@@ -0,0 +1,24 @@
+# Describe the difference between ! and ? in Ruby. And explain what would happen in the following scenarios:
+
+=begin
+  1. what is != and where should you use it?
+    Answer: This is the not equivalent operator in Ruby syntax. This is used
+    in a conditional statement.
+    
+    Result:
+    
+  2. put ! before something, like !user_name
+    Answer: Negation operator. Example: !true = false.
+    
+    Result:
+    
+  3. put ! after something, like words.uniq!
+    Answer: Mutates the caller or argument (original memory space).
+    
+    Result:
+    
+  4. put ? before something
+    Answer: 
+  5. put ? after something
+  6. put !! before something, like !!user_name
+=end
